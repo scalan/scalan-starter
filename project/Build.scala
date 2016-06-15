2d88733ced228131383a3fdc03ea8ad2e6df3e48
Alexey Romanov <alexey.romanov@huawei.com>
2016-06-15 11:54:53 +0300

    Update build

--- project/Build.scala
+++ project/Build.scala
@@ -2,55 +2,30 @@ import sbt._
 import sbt.Keys._
 
 object Build extends Build {
-  val commonDeps = libraryDependencies ++= Seq(
-    "org.scalatest" %% "scalatest" % "2.2.1" % "test",
-    "org.scalacheck" %% "scalacheck" % "1.11.5" % "test")
-
-  val testSettings = inConfig(ItTest)(Defaults.testTasks) ++ Seq(
+  val testSettings = Defaults.itSettings ++ Seq(
     // needed thanks to http://stackoverflow.com/questions/7898273/how-to-get-logging-working-in-scala-unit-tests-with-testng-slf4s-and-logback
     parallelExecution in Test := false,
-    parallelExecution in ItTest := false,
-    publishArtifact in Test := true,
+    parallelExecution in IntegrationTest := false,
     javaOptions in Test ++= Seq("-Xmx10G", "-Xms5G"),
-    publishArtifact in(Test, packageDoc) := false
+    javaOptions in IntegrationTest ++= Seq("-Xmx10G", "-Xms5G"),
+    libraryDependencies ++= Seq("org.scalatest" %% "scalatest" % "2.2.6" % "test;it")
   )
 
-  val buildSettings = Seq(
-    organization := "com.huawei.scalan",
-    scalaVersion := "2.11.8",
-    scalacOptions ++= Seq(
-      "-unchecked", "-deprecation",
-      "-feature",
-      "-language:higherKinds",
-      "-language:implicitConversions",
-      "-language:existentials",
-      "-language:postfixOps"))
-
-  lazy val noPublishingSettings = Seq(
-    publishArtifact := false,
-    publish := {},
-    publishLocal := {})
-
-  override lazy val settings = super.settings ++ buildSettings
-
   lazy val commonSettings =
-    buildSettings /*++ assemblySettings ++ releaseSettings*/ ++ testSettings ++
-      Seq(
-      //resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
-      publishTo := {
-        val nexus = "http://10.122.85.37:9081/nexus/"
-        if (version.value.trim.endsWith("SNAPSHOT"))
-          Some("snapshots" at (nexus + "content/repositories/snapshots"))
-        else
-          Some("releases" at (nexus + "content/repositories/releases"))
-      },
-      commonDeps)
+    Seq(
+      organization := "com.huawei.scalan",
+      scalaVersion := "2.11.8",
+      scalacOptions ++= Seq(
+        "-unchecked", "-deprecation", "-feature",
+        "-language:higherKinds", "-language.existentials"
+      )
+    ) ++ testSettings /*++ assemblySettings */
 
   implicit class ProjectExt(p: Project) {
     def allConfigDependency = p % "compile->compile;test->test"
 
     def addTestConfigsAndCommonSettings =
-      p.configs(ItTest).settings(commonSettings: _*)
+      p.configs(IntegrationTest).settings(commonSettings: _*)
   }
 
   def scalanDependency(name: String) = "com.huawei.scalan" %% name % "0.3.0-SNAPSHOT"
@@ -71,8 +46,6 @@ object Build extends Build {
     id = "scalan-starter-core",
     base = file("scalan-starter-core")).addTestConfigsAndCommonSettings.
     settings(libraryDependencies ++= Seq(
-      "commons-io" % "commons-io" % "2.5",
-      "com.github.kxbmap" %% "configs-java7" % "0.3.0",
       scalanCommon, scalanCommon % "test" classifier "tests",
       scalanCore, scalanCore % "test" classifier "tests",
       scalanCollections, scalanCollections % "test" classifier "tests",
@@ -84,22 +57,7 @@ object Build extends Build {
     id = "scalan-starter",
     base = file(".")).addTestConfigsAndCommonSettings
     .aggregate(meta, core)
-    .settings(
-      libraryDependencies ++= Seq(
-        scalanCore, scalanCore % "test" classifier "tests",
-        scalanCollections, scalanCollections % "test" classifier "tests",
-        scalanLA, scalanLA % "test" classifier "tests"
-        ),
-      publishArtifact := false)
-
-  def itFilter(name: String): Boolean =
-    name endsWith "ItTests"
-
-  def unitFilter(name: String): Boolean = !itFilter(name)
-
-  lazy val ItTest = config("it").extend(Test)
-
-  publishArtifact in Test := true
+    .settings(publishArtifact := false)
 
   publishArtifact in packageDoc := !version.value.trim.endsWith("SNAPSHOT")
 
