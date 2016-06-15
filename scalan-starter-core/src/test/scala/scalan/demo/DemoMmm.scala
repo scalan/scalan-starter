4c526cfb0ea7814d3d71c6e3527bddef1a336b21
Alexey Romanov <alexey.romanov@huawei.com>
2016-06-15 13:34:52 +0300

    Mark C++ matrix-matrix multiplication as pending

--- scalan-starter-core/src/test/scala/scalan/demo/DemoMmm.scala
+++ scalan-starter-core/src/test/scala/scalan/demo/DemoMmm.scala
@@ -18,33 +18,21 @@ class DemoMmm extends BaseNestedTests {
       print("r", res)
     }
 
-    it("Staged evaluation") {
+    describe("Staged evaluation") {
       val ctx = new ExampleMmmExp
       import ctx._
 
       val kernelsDir = new File("./test-out/Demo2Exp/mmm")
       val kstore = KernelStore.open(ctx, kernelsDir)
 
-      val (dmdmA_s, _) = time("Generating Scala kernel") {
-        val k = kstore.createKernel("dmdmA", KernelType.Scala, dmdmA)
-        k((dmData, dmData))
-        k
-      }
-
-      val (dmdmA_c, _) = time("Generating C++ kernel") {
-        val k = kstore.createKernel("dmdmA", KernelType.Cpp, dmdmA)
-        k((dmData, dmData))
-        k
-      }
-
       val rows = 500
       val cols = rows
 
-      val (a,_) = time( s"""Generating ${rows}x${cols} Array[Array[Double]] ...""") {
+      val (a,_) = time( s"""Generating first ${rows}x${cols} Array[Array[Double]] ...""") {
         genMatr(rows, cols)
       }
 
-      val (bT, _) = time( s"""Generating ${rows} Array[Double] ...""") {
+      val (bT, _) = time( s"""Generating second ${rows}x${cols} Array[Array[Double]] ...""") {
         genMatr(rows, cols)
       }
 
@@ -53,16 +41,35 @@ class DemoMmm extends BaseNestedTests {
           a.map { aRow =>
             aRow.zip(bColumn)
               .map { case (x,y) => x * y }
-              .reduce(_ + _)
+              .sum
           }
         }
       }
 
-      val (resS,_) = time(s"Executing Scala kernel") { dmdmA_s((a, bT)) }
-      val (resC,_) = time(s"Executing C++ kernel") { dmdmA_c((a, bT)) }
+      it("Scala") {
+        val (dmdmA_s, _) = time("Generating Scala kernel") {
+          val k = kstore.createKernel("dmdmA", KernelType.Scala, dmdmA)
+          k((dmData, dmData))
+          k
+        }
 
-      assertResult(correctRes)(resS)
-      assertResult(correctRes)(resC)
+        val (resS,_) = time(s"Executing Scala kernel") { dmdmA_s((a, bT)) }
+
+        assertResult(correctRes)(resS)
+      }
+
+      // generated C++ code is currently incorrect
+      ignore("C++") {
+        val (dmdmA_c, _) = time("Generating C++ kernel") {
+          val k = kstore.createKernel("dmdmA", KernelType.Cpp, dmdmA)
+          k((dmData, dmData))
+          k
+        }
+
+        val (resC, _) = time(s"Executing C++ kernel") {dmdmA_c((a, bT))}
+
+        assertResult(correctRes)(resC)
+      }
     }
   }
 }
